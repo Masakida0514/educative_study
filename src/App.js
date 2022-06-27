7a9ece4302101d5da3d3c79affc17dff0ceccd06
Masakida0514 <masakida0514@akane.waseda.jp>
2022-06-28 00:13:22 +0900

    React Stateを用いて、検索欄のinputをリアルタイムで受け取る

--- src/App.js
+++ src/App.js
@@ -1,48 +1,37 @@
 import React from 'react';
 
-// グローバル変数
-// 今回はapp内で変数を定義し、他のクラスに渡す：Props
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+// React Props : コンポーネントツリーに情報を渡す際に使う
+// React State : アプリケーションをインタラクティブにする際に使う
 
 const App = () => {
-const stories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  // 便利な関数、useStateを使う
+  // これはReactからmanaging stateを取得する
+  // useStateは別名 hook を呼ばれる
+  const [searchTerm, setSearchTerm] = React.useState('');
+  // 1st termはcurrent stateを表す
+  // 2nd termは、このcurrent stateをupdateするstate
 
   const handleChange = event => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
 
 
@@ -53,16 +42,19 @@ const stories = [
       <label htmlFor='search'>検索: </label>
       <input id="search" type="text" onChange={handleChange}/>
 
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+
       <hr />
-      {/* List()にlistとしてstoriesをpropsしてる */}
       <List list = {stories}/>
     </div>
   );
 }
 
 
-const List = () => 
-  list.map(item => (
+const List = props => 
+  props.list.map(item => (
     <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
